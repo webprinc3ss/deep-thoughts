24ebed0044adc2e1d8ef7720fc5f10cc35eace64
Mystic Kitty <djabranton@gmail.com>
2021-02-24 19:38:45 -0800

    Updated profile.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,6 +8,8 @@ import FriendList from '../components/FriendList';
 const Home = () => {
   // use useQuery hook to make query request
   const { loading, data } = useQuery(QUERY_THOUGHTS);
+  // use object destructuring to extract `data` from the `useQuery` Hook's response and rename it `userData` to be more descriptive
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
   const thoughts = data?.thoughts || [];
   console.log(thoughts);
   const loggedIn = Auth.loggedIn();
@@ -21,6 +23,15 @@ const Home = () => {
               <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
             )}
         </div>
+        {loggedIn && userData ? (
+          <div className="col-12 col-lg-3 mb-3">
+            <FriendList
+              username={userData.me.username}
+              friendCount={userData.me.friendCount}
+              friends={userData.me.friends}
+            />
+          </div>
+        ) : null}
       </div>
     </main>
   );
