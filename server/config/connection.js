d11ef91b464bad5327028ed1a4dddf09cd79a5fc
Mystic Kitty <djabranton@gmail.com>
2021-02-24 21:42:06 -0800

    Updated package.json to include heroku script.

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
   {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
